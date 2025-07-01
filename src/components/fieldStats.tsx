dc3cc7cb7491c86dd98bc70c238fb68a95c2a6bf
vishnu-mukati <mukativishnu1232@gmaila.com>
2025-07-01 19:24:07 +0530

    adding some colors and styles

--- src/components/fieldStats.tsx
+++ src/components/fieldStats.tsx
@@ -44,7 +44,7 @@ export const FieldStates = () => {
 
       const interval = setInterval(() => {
         getWork();
-      }, 5000);
+      }, 10000);
 
       return () => clearInterval(interval);
     }
@@ -76,14 +76,12 @@ export const FieldStates = () => {
           safeUserEmail,
           field.id
         );
-        console.log(response2.data);
         if (response2.data) {
           const fieldWorkItems = Object.keys(response2.data).map((workId) => ({
             ...response2.data[workId],
             id: workId,
             fieldId: field.id,
           }));
-          console.log(fieldWorkItems);
           allFieldWorkItems.push(...fieldWorkItems);
         }
       } catch (err) {
@@ -106,33 +104,50 @@ export const FieldStates = () => {
 
   return (
     <Box>
-      <Typography variant="h1" color="black">
-        Hii this is the stats
-      </Typography>
-
-      <Box>
-        <TableContainer>
+      <Box bgcolor="floralwhite">
+        <Typography variant="h4" color="black">
+          Cost of the fields
+        </Typography>
+      </Box>
+      <Box boxShadow={2}>
+        <TableContainer sx={{ borderRadius: 2, boxShadow: 3 }}>
           <Table>
             <TableHead>
-              <TableRow>
-                <TableCell>S.No</TableCell>
-                <TableCell>Fields</TableCell>
-                <TableCell>Field Total Cost</TableCell>
+              <TableRow sx={{ backgroundColor: "#f0f4f8" }}>
+                <TableCell sx={{ fontWeight: "bold", color: "#2c3e50" }}>
+                  S.No
+                </TableCell>
+                <TableCell sx={{ fontWeight: "bold", color: "#2c3e50" }}>
+                  Fields
+                </TableCell>
+                <TableCell sx={{ fontWeight: "bold", color: "#2c3e50" }}>
+                  Field Total Cost
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {fieldListData.length > 0 ? (
                 fieldListData.map((field, id) => (
-                  <TableRow key={field.id}>
-                    <TableCell>{id + 1}</TableCell>
-                    <TableCell>{field.fieldName}</TableCell>
-                    <TableCell>{getTotalCostForField(field.id)}</TableCell>
+                  <TableRow
+                    key={field.id}
+                    sx={{
+                      "&:nth-of-type(odd)": {
+                        backgroundColor: "#fafafa",
+                      },
+                      "&:hover": {
+                        backgroundColor: "#e3f2fd",
+                      },
+                    }}
+                  >
+                    <TableCell align="center">{id + 1}</TableCell>
+                    <TableCell align="center">{field.fieldName}</TableCell>
+                    <TableCell align="center">{getTotalCostForField(field.id)}</TableCell>
                   </TableRow>
                 ))
               ) : (
                 <TableRow>
-                  <TableCell>
-                    <Typography variant="h6">No Data Available</Typography>
+                  <TableCell colSpan={3}>
+                    <Typography variant="h6" align="center" sx={{ color: "#7f8c8d" }}>No Data Available</Typography>
                   </TableCell>
                 </TableRow>
               )}
